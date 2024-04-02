aaec0e6b22c3f0a0037333842984da5f09d1181e
Plamen Atanasov <plamen.atanasov25@gmail.com>
2024-04-02 18:19:03 +0300

    Add Rule of four methods, logic for reading/writing from binary file and adding users

---
+++ MagicTheGathering/Players.cpp
@@ -0,0 +1,118 @@
+#include "Players.h"
+
+#include <fstream>
+#include <cassert>
+
+Players::Players(const char* fileName)
+{
+	if (fileName == nullptr || *fileName == '\0') {
+		throw std::invalid_argument("file name is not valid");
+	}
+
+	this->fileName = new char[strlen(fileName) + 1];
+	strcpy_s(this->fileName, strlen(fileName) + 1, fileName);
+
+	readPlayers();
+}
+
+Players::Players(const Players& other) {
+	copyFrom(other);
+}
+
+Players& Players::operator=(const Players& other) {
+	if (this != &other) {
+		free();
+		copyFrom(other);
+	}
+	return *this;
+}
+
+Players::~Players() {
+	writePlayers();
+	free();
+}
+
+const Player* Players::getPlayers() const {
+	return players;
+}
+
+size_t Players::getPlayersCount() const {
+	return size;
+}
+
+void Players::printPlayers() const {
+	for (int i = 0; i < size; i++) {
+		std::cout << players[i].name << '\n';
+	}
+	std::cout << std::endl;
+}
+
+void Players::addPlayer(const Player& player) {
+	if (size == capacity) {
+		resize();
+	}
+
+	players[size++] = player;
+}
+
+void Players::readPlayers() {
+	std::ifstream ifile(fileName, std::ios::binary);
+	if (!ifile.is_open()) {
+		return;
+	}
+
+	ifile.read(reinterpret_cast<char*>(&size), sizeof(size));
+	capacity = size < 8 ? 8 : size;
+	players = new Player[capacity];
+	ifile.read(reinterpret_cast<char*>(players), static_cast<long long>(size) * sizeof(Player));
+
+	ifile.close();
+}
+
+void Players::writePlayers() {
+	std::ofstream ofile(fileName, std::ios::binary);
+	if (!ofile.is_open()) {
+		throw std::exception("file could not be opened");
+	}
+
+	ofile.write(reinterpret_cast<const char*>(&size), sizeof(size));
+	ofile.write(reinterpret_cast<const char*>(players), static_cast<long long>(size) * sizeof(Player));
+
+	ofile.close();
+}
+
+void Players::resize() {
+	size_t newCapacity = capacity < 8 ? 8 : capacity * 2;
+	Player* newPlayers = new Player[newCapacity];
+	for (int i = 0; i < size; i++) {
+		newPlayers[i] = players[i];
+	}
+
+	delete[] players;
+	players = newPlayers;
+	capacity = newCapacity;
+}
+
+void Players::free() {
+	delete[] players;
+	players = nullptr;
+	size = capacity = 0;
+	delete[] fileName;
+	fileName = nullptr;
+}
+
+void Players::copyFrom(const Players& other) {
+	if (other.capacity > 0) {
+		players = new Player[other.capacity];
+		assert(other.size <= other.capacity);
+		for (int i = 0; i < other.size; i++) {
+			players[i] = other.players[i];
+		}
+
+		size = other.size;
+		capacity = other.capacity;
+	}
+
+	fileName = new char[strlen(other.fileName) + 1];
+	strcpy_s(fileName, strlen(other.fileName) + 1, other.fileName);
+}
---
+++ MagicTheGathering/Players.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <iostream>
+
+#include "Utils.h"
+
+class Players {
+public:
+	Players(const char* fileName);
+	Players(const Players& other);
+	Players& operator=(const Players& other);
+	~Players();
+
+	const Player* getPlayers() const;
+	size_t getPlayersCount() const;
+	void printPlayers() const;
+
+	void addPlayer(const Player& player);
+private:
+	void readPlayers();
+	void writePlayers();
+	void resize();
+	void free();
+	void copyFrom(const Players& other);
+
+	Player* players = nullptr;
+	size_t size = 0;
+	size_t capacity = 0;
+
+	char* fileName = nullptr;
+};
